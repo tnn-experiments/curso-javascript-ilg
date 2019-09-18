be3de3bc5e5e41cf6e8ecc95c074e039c06db61a
theandersonn <theandersonn@gmail.com>
2019-09-18 07:08:54 -0300

    Add 20 Loops while e do/while

---
+++ js/20.js
@@ -0,0 +1,18 @@
+// Loops while e do/while
+
+
+// exemplo 1
+var count = 0;
+
+while (count < 5) {
+    console.log(count);
+    count++;
+}
+
+// exemplo 2
+var count2 = 0;
+    
+do {
+    console.log(count2);
+    count2++;
+} while (count2 < 5);
