d531fa3a34cc71e33e48a7764a6b8974d0b2e1bd
theandersonn <theandersonn@gmail.com>
2019-09-23 06:55:56 -0300

    Add aula 25b

--- js/25.js
+++ js/25.js
@@ -9,11 +9,36 @@ console.log(localStorage['nome']);
 
 // exemplo 2
 document.getElementById('enviar-nome').onclick = function() {
+    // guardar o nome digitado em local storage
     var nome = document.getElementById('nome-usuario').value;
     localStorage.setItem('nome', nome);
 
+    // esconder o formulário
     document.getElementById('name-field').style.display = 'none';  
+    
+    // atualizar e mostrar mensagem de boas vindas
     document.getElementById('welcome-text').innerHTML = 'Olá ' + localStorage.nome + ', tudo bem?';  
     document.getElementById('not-me').innerHTML = 'Não é ' + localStorage.nome + '?';  
     document.getElementById('welcome-area').style.display = 'initial';  
+};
+
+if(localStorage.nome) {
+    // esconder o formulário
+    document.getElementById('name-field').style.display = 'none';  
+
+    // atualizar e mostrar mensagem de boas vindas
+    document.getElementById('welcome-text').innerHTML = 'Olá ' + localStorage.nome + ', tudo bem?';  
+    document.getElementById('not-me').innerHTML = 'Não é ' + localStorage.nome + '?';  
+    document.getElementById('welcome-area').style.display = 'initial';      
+};
+
+document.getElementById('not-me').onclick = function() {
+    // remover chave nome do local storage
+    localStorage.removeItem('nome');
+
+    // esconder mensagem de boas vindas
+    document.getElementById('welcome-area').style.display = 'none';
+
+    // mostrar formulário
+    document.getElementById('name-field').style.display = 'initial';
 };
