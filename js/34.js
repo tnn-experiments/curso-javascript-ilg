43b1bfaccf4d2692db27fd4ce076dba4c7652b5e
theandersonn <theandersonn@gmail.com>
2019-10-07 06:39:57 -0300

    aula 34 - jQuery - Sintaxe

---
+++ js/34.js
@@ -0,0 +1,10 @@
+// jQuery - Sintaxe
+// --------------------
+
+// $('#esconder').click(function(){
+//     $('.exemplo').css('display', 'none');
+// });
+
+$('#esconder').on('click', function(){
+    $('.exemplo').hide();
+});
