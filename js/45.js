613143db41414c47879fa5eee2e369a6e27a0a9d
theandersonn <theandersonn@gmail.com>
2019-10-09 21:28:05 -0300

    45 - Operador If Ternário

---
+++ js/45.js
@@ -0,0 +1,12 @@
+// Operador If Ternário
+// --------------------------
+
+// condição ? "valor se for verdadeiro" : "valor se for falso";
+
+var nome = 'theandersonn';
+var mensagem;
+
+// nome ? mensagem = 'Olá ' + nome : mensagem = 'Nome não informado';
+mensagem = nome ? 'Olá ' + nome : 'Nome não informado';
+
+console.log(mensagem);
