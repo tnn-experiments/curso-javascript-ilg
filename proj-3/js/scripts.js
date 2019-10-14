a73e023b3a0bb4e6e7752d636f20d664e4f9eed8
theandersonn <theandersonn@gmail.com>
2019-10-14 07:23:31 -0300

    87 - Pegar o código do local com o geocode API

--- proj-3/js/scripts.js
+++ proj-3/js/scripts.js
@@ -12,16 +12,40 @@ $(function(){
 
 var accuweatherAPIKey = 'gRTwpZJ2ZR05SIgO9Z2mWenYRu8sMkH7';
 
-$.ajax({
-    url: 'http://dataservice.accuweather.com/currentconditions/v1/45881?apikey='+ accuweatherAPIKey +'&language=pt-br',
-    type: 'GET',
-    dataType: 'json',
-    success: function(data){
-        console.log(data);
-    },
-    error: function(){
-        console.log('erro');
-    }
-});
+function pegarTempoAtual(localCode) {
+
+    $.ajax({
+        url: 'http://dataservice.accuweather.com/currentconditions/v1/' + localCode + '?apikey=' + accuweatherAPIKey + '&language=pt-br',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+            console.log(data);
+        },
+        error: function(){
+            console.log('erro');
+        }
+    });
+
+}
+
+function pegarLocalUsuario(lat, long) {
+
+    $.ajax({
+        url: 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=' + accuweatherAPIKey + '&q=' + lat + '%2C' + long + '&language=pt-br',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+            var localCode = data.Key; 
+            pegarTempoAtual(localCode);
+        },
+        error: function(){
+            console.log('erro');
+        }
+    });
+
+}
+
+pegarLocalUsuario(-22.929116, -43.172736);
+
 
 });
